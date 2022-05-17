00cd6f525a4f088205bc2e6d44e7c87b0151dc26
Steve Tosh <giftedtosh@gmail.com>
2022-05-17 10:38:37 +0100

    init

--- script/scripts.js
+++ script/scripts.js
@@ -1,3 +1,4 @@
+/*  hamburger Toggler */
 const hamburger = document.getElementById('hamburger'),
 navul = document.querySelector('.navul');
 
@@ -6,7 +7,7 @@ hamburger.addEventListener('click', ()=>{
 });
 
 
-//SELECT ELEMENETS FROM DOM
+/* Psswords Toggler */
 const pass = document.querySelector('#password');
 const pass2 = document.querySelector('#confirm-password');
 const toggle = document.querySelector('#toggle');
@@ -20,15 +21,34 @@ toggle.addEventListener('click', ()=>{
     // toggle2.classList.toggle('fa-eye-slash');
 });
 
-
+/* Passwords Validation */
 var password = document.getElementById('password');
 confirm_password = document.getElementById('confirm-password');
     function validatePassword() {
         if ((confirm_password.value!='')&&(password.value != confirm_password.value)) {
             document.getElementById('errorMsg').innerHTML='Passwords Don\'t Match';
-        } else {
+        } 
+        /*else if(password.value === confirm_password.value) {
+          var msg=document.getElementById('errorMsg').innerHTML='Perfect Password';
+          msg.style.color='green';
+        }*/
+        else {
             document.getElementById('errorMsg').innerHTML='';
         }
-    }
+      }
     password.onchange = validatePassword;
     confirm_password.onkeyup = validatePassword;
+
+
+/* var password = document.getElementById('password'),
+  confirm_password = document.getElementById('confirm-password');
+
+function validatePassword() {
+  if (password.value !== confirm_password.value) {
+    confirm_password.setCustomValidity("Passwords Don't Match");
+  } else {
+    confirm_password.setCustomValidity('');
+  }
+}
+password.oninput = validatePassword;
+confirm_password.oninput = validatePassword;*/
