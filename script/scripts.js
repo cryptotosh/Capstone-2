b11649c4dd940e063e324442ec89689439697863
Steve Tosh <giftedtosh@gmail.com>
2022-05-17 07:21:20 +0100

    init

--- script/scripts.js
+++ script/scripts.js
@@ -1,6 +1,34 @@
-const hambugger = document.getElementById('hambugger'),
+const hamburger = document.getElementById('hamburger'),
 navul = document.querySelector('.navul');
 
-hambugger.addEventListener('click', ()=>{
+hamburger.addEventListener('click', ()=>{
     navul.classList.toggle('show');
 });
+
+
+//SELECT ELEMENETS FROM DOM
+const pass = document.querySelector('#password');
+const pass2 = document.querySelector('#confirm-password');
+const toggle = document.querySelector('#toggle');
+const toggle2 = document.querySelector('#toggle2');
+
+toggle.addEventListener('click', ()=>{
+   const showPass = pass.getAttribute('type')=== 'text';
+    pass.setAttribute('type', showPass ? 'password' : 'text');
+    pass2.setAttribute('type', showPass ? 'password' : 'text');
+    toggle.classList.toggle('fa-eye-slash');
+    // toggle2.classList.toggle('fa-eye-slash');
+});
+
+
+var password = document.getElementById('password');
+confirm_password = document.getElementById('confirm-password');
+    function validatePassword() {
+        if ((confirm_password.value!='')&&(password.value != confirm_password.value)) {
+            document.getElementById('errorMsg').innerHTML='Passwords Don\'t Match';
+        } else {
+            document.getElementById('errorMsg').innerHTML='';
+        }
+    }
+    password.onchange = validatePassword;
+    confirm_password.onkeyup = validatePassword;
